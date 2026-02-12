db03a296cb78b54aed9deb29e24aebbfcdd74db8
Fatnassi Fares <fatnassifares@Host-006.lan>
2026-02-12 11:20:16 +0100

    first commit

---
+++ src/pages/login/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {Link}   from "react-router-dom";
+export default function Login() {
+  const [user, setUser] = React.useState({ email: "", password: "" });
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <div className="container">
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card_title">
+          <h1>Login Here</h1>
+        </div>
+        <div className="form">
+          <form onSubmit={onFormSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="card_terms">
+          <span>
+            Don't have an account yet?
+            <Link to="/signup">Signup Here</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
